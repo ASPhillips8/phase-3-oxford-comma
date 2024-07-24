4f5f25c1bc9dd2a49fc74249cc4d85ce292bdab6
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-24 13:27:13 -0400

    Create Oxford Comma Method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+ case array.length
+ when 0
+  ""
+ when 1
+  array.first
+ when 2
+  array.join(" and ")
+ else
+  array[-1] = "and #{array.last}"
+  array.join(", ")
+ end
 end
